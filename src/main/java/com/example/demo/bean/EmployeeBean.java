836f860b40397a609500e3e51d56eb14a2284b09
Padmalochan Sutar <padmalochan.sutar@CORP.CSMPL.COM>
2022-12-13 16:31:17 +0530

    add setter and getter

--- src/main/java/com/example/demo/bean/EmployeeBean.java
+++ src/main/java/com/example/demo/bean/EmployeeBean.java
@@ -6,6 +6,30 @@ private Integer empId;
 	private String empName;
 	
 	private Integer age;
+//(HIS-212)
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 	public EmployeeBean(Integer empId, String empName, Integer age) {
 		super();
